ced23663dcd421d0c1799ba438508c231e021e76
Mahmoud Emad <mahmoud.is.here.28@gmail.com>
2025-02-17 21:14:15 +0200

    AdminOrUser check

--- Airport_proj.c
+++ Airport_proj.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 int main(){
-    printf("GOD of War!!!\n");
+    char UserOrAdmin = 0;
+    printf("Enter 1 for Admins, 2 for users: ");
+    UserAdminInvalid:
+    scanf("%d", &UserOrAdmin);
+    switch(UserOrAdmin){
+        case 1:
+        printf("Iam an admin\n");
+        break;
+        case 2:
+        printf("Iam a user\n");
+        break;
+        default:
+        printf("Invalid input!!\n");
+        goto UserAdminInvalid;
+        break;
+    }
     return 0;
 }
